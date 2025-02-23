10eb72fbef327482c25c2b5835c72b068f78af4e
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-02-23 11:56:31 -0700

    print extensions

--- include/vulkan_helper.hpp
+++ include/vulkan_helper.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <expected>
+#include <print>
 #include <string_view>
 #include <tuple>
+#include <vector>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -13,6 +16,14 @@
 constexpr uint32_t WINDOW_WIDTH = 800;
 constexpr uint32_t WINDOW_HEIGHT = 800;
 constexpr auto title = "hello_vulkan";
+
+#ifndef NDEBUG
+constexpr auto validation_layers_enabled = false;
+#else
+constexpr auto validation_layers_enabled = true;
+#endif
+
+std::array<const char*, 1> validation_layers = {"VK_LAYER_KHRONOS_validation"};
 
 auto initialize_window(const uint32_t width, const uint32_t height,
                        std::string_view title) noexcept
@@ -37,7 +48,8 @@ auto initialize_window(const uint32_t width, const uint32_t height,
   return window;
 }
 
-constexpr auto get_vulkan_application_information() noexcept -> VkApplicationInfo {
+constexpr auto get_vulkan_application_information() noexcept
+    -> VkApplicationInfo {
   auto application_information = VkApplicationInfo{};
   application_information.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   application_information.pApplicationName = "hello vulkan";
@@ -48,18 +60,25 @@ constexpr auto get_vulkan_application_information() noexcept -> VkApplicationInf
   return application_information;
 }
 
-auto get_glfw_extensions() noexcept -> std::tuple<const char* const*, uint32_t> {
+auto get_glfw_extensions() noexcept
+    -> std::tuple<const char* const*, uint32_t> {
   uint32_t glfw_extensions_count = 0;
   auto glfw_extensions =
       glfwGetRequiredInstanceExtensions(&glfw_extensions_count);
+
+  std::println("GLFW extensions");
+  for (uint32_t i = 0; i < glfw_extensions_count; ++i) {
+    std::println("\t{}", glfw_extensions[i]);
+  }
+  std::println();
 
   return std::tuple(glfw_extensions, glfw_extensions_count);
 }
 
 constexpr auto get_vulkan_create_instance_information(
     VkApplicationInfo* application_information,
-    const char* const* glfw_extensions, uint32_t glfw_extensions_count)
-    noexcept -> VkInstanceCreateInfo {
+    const char* const* glfw_extensions, uint32_t glfw_extensions_count) noexcept
+    -> VkInstanceCreateInfo {
   auto create_instance_information = VkInstanceCreateInfo{};
   create_instance_information.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
   create_instance_information.pApplicationInfo = application_information;
@@ -67,6 +86,22 @@ constexpr auto get_vulkan_create_instance_information(
   create_instance_information.enabledExtensionCount = glfw_extensions_count;
   create_instance_information.enabledLayerCount = 0;
   return create_instance_information;
+}
+
+auto get_vulkan_extensions() noexcept -> std::vector<VkExtensionProperties> {
+  uint32_t extension_count = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+
+  std::vector<VkExtensionProperties> extensions(extension_count);
+  vkEnumerateInstanceExtensionProperties(nullptr, &extension_count,
+                                         extensions.data());
+
+  std::println("available vulkan extensions:");
+  for (const auto& extension : extensions) {
+    std::println("\t{}", extension.extensionName);
+  }
+
+  return extensions;
 }
 
 auto initialize_vulkan() noexcept -> std::expected<VkInstance, Error> {
@@ -91,10 +126,13 @@ auto initialize_vulkan() noexcept -> std::expected<VkInstance, Error> {
     return std::unexpected(Error::failed_to_initialize_vulkan(result));
   }
 
+  get_vulkan_extensions();
+
   return instance;
 }
 
-auto initialize() noexcept -> std::expected<std::tuple<GLFWwindow*, VkInstance>, Error> {
+auto initialize() noexcept
+    -> std::expected<std::tuple<GLFWwindow*, VkInstance>, Error> {
   GLFWwindow* window = nullptr;
   if (auto result = initialize_window(WINDOW_WIDTH, WINDOW_HEIGHT, title);
       result.has_value()) {
